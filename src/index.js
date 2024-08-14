441e98d96af03ae6fe7572bfd59b6b973f1d7ab3
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-08-14 17:39:49 -0600

    create functions to render webpages, and add eventlisteners to menu

--- src/index.js
+++ src/index.js
@@ -3,7 +3,42 @@ import createMenu from "./menu";
 import createContact from "./contact";
 import "./styles.css"
 
+function clearContent() {
+    const content = document.getElementById("content");
+    content.innerHTML = "";
+}
 
-createHome();
-createMenu();
-createContact();
+function loadHome() {
+    clearContent();
+    createHome();
+};
+
+function loadMenu() {
+    clearContent();
+    createMenu();
+};
+
+function loadContact() {
+    clearContent();
+    createContact();
+}
+
+function eventListeners() {
+    const homeBtn = document.querySelector("button:nth-child(1)");
+    homeBtn.addEventListener("click", loadHome);
+
+    const menuBtn = document.querySelector("button:nth-child(2)");
+    menuBtn.addEventListener("click", loadMenu);
+
+    const contactBtn = document.querySelector("button:nth-child(3)");
+    contactBtn.addEventListener("click", loadContact);
+}
+eventListeners();
+
+function initialize() {
+    document.addEventListener("DOMContentLoaded", () => {
+        eventListeners();
+        loadHome();
+    });
+}
+initialize();
